ef473dee0d13ba8871e72901be31f811d47cf83a
jlongyam <jhonliongbox@gmail.com>
2025-07-27 02:11:11 +0700

    delete 1 file, create 28 files, update 3 files and move 8 files

--- src/input.js
+++ src/input.js
@@ -1,7 +1,13 @@
 const { default: Enquirer } = await import('../node_modules/enquirer/index.js');
 
 const enquirer = new Enquirer();
-
+/**
+ * Prompts the user with an input question using Enquirer and logs the response.
+ *
+ * @param {Object} option - Contains the prompt configuration.
+ * @param {string} option.key - The key for the response object.
+ * @param {string} option.question - The message to display to the user.
+ */
 async function ask(option) {
   const response = await enquirer.prompt({
     type: 'input',
@@ -10,7 +16,12 @@ async function ask(option) {
   });
   console.log(response);
 }
-
+/**
+ * Prompts the user with a series of input questions based on the provided options array using Enquirer.
+ *
+ * @param {Array} options - An array of objects, each containing a 'key' and 'question' property for the prompt.
+ * @returns {Promise<void>} Logs the user's responses to the console.
+ */
 async function asks(options) {
   const response = await enquirer.prompt((function () {
     let arrayObject = [];
