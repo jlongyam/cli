87bac860f41b24a974cb3e756423f1066824bfa8
jlongyam <jhonliongbox@gmail.com>
2025-07-27 02:32:27 +0700

    copy 1 file and update 7 files

--- src/args.js
+++ src/args.js
@@ -1,3 +1,11 @@
 const { default: Parse } = await import('../node_modules/args-parser/parse.js');
-
+/**
+ * Imports the Parse function from the args-parser module and exports the parsed command-line arguments as an optional 'args' constant.
+ * @example
+ * if(Object.keys(args).length === 0) {
+ *   console.log('no arguments')
+ * } else {
+ *   console.log(args)
+ * }
+ */
 export const args = Parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { args } from './args.js';
 import { input } from './input.js';
-
+/**
+ * @returns {Object}
+ * @example
+ * import { args } from "@jlongyam/cli";
+ * console.log(args);
+ */
 export {
   args,
   input
